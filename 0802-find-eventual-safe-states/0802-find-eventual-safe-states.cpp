0295fd502e5a3277eac052a073594f1693b7b99f
Medha-Naik <medham.23.becs@acharya.ac.in>
2026-01-20 00:17:34 +0530

    Time: 3 ms (94.12%), Space: 52.4 MB (80.41%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-    bool dfs(int node,vector<vector<int>>&graph,vector<int>&vis,vector<int>&pathvis,vector<int>&check)
+    bool dfs(int node,vector<vector<int>>&graph,vector<int>&vis,vector<int>&color)
     {
         vis[node]=1;
-        pathvis[node]=1;
-        check[node]=0;
+        color[node]=1;
         for(auto it:graph[node])
         {
-            if(!vis[it])
-            {
-                if(dfs(it,graph,vis,pathvis,check)==true)
-                {
-                    check[node]=0;
-                    return true;
-                }
-            }
-            else if(pathvis[it])
-            {
-                check[node]=0;
-                return true;
-            }
+            if(color[it]==1)return true;
+            if(color[it]==0&&dfs(it,graph,vis,color))return true;
         }
-            check[node]=1;
-            pathvis[node]=0;
-            return false;
-        
+        color[node]=2;
+        return false;
     }
-
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-       int v=graph.size();
-       vector<int>vis(v,0);
-       vector<int>pathvis(v,0);
-       vector<int>check(v,0);
-       vector<int>safenodes;
-       for(int i=0;i<v;i++)
-       {
-        if(!vis[i])dfs(i,graph,vis,pathvis,check);
-       }
-       for(int i=0;i<v;i++)
-       {
-        if(check[i]==1)safenodes.push_back(i);
-       }
-       return safenodes;
+        int n=graph.size();
+        vector<int>vis(n,0);
+        vector<int>color(n,0);
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])dfs(i,graph,vis,color);
+        }
+        vector<int>sol;
+        for(int i=0;i<n;i++)
+        {
+            if(color[i]==2)sol.push_back(i);
+        }
+        return sol;
     }
 };
