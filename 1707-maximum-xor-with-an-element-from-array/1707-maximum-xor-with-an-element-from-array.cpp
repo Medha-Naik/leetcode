b9ab7000902a58bee1186e080ac7d335711ef215
Medha-Naik <medham.23.becs@acharya.ac.in>
2026-02-04 00:08:08 +0530

    Time: 407 ms (63.74%), Space: 222.3 MB (62.71%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
@@ -1,56 +1,41 @@
 struct Node{
-    Node * bits[2];
     Node * links[2];
-
     Node()
     {
         links[0]=links[1]=nullptr;
-    }
-    bool containsKey(int bit)
-    {
-        return links[bit]!=NULL;
-    }
-    Node *get(int bit)
-    {
-        return links[bit];
-    }
-    void put(int bit, Node * node)
-    {
-        links[bit]=node;
     }
 };
 
 class Solution {
 public:
+
     Node * root;
     Solution()
     {
         root=new Node();
     }
-       void insert(int num)
-        {
-            Node * node=root;
-            for(int i=31;i>=0;i--)
+
+    void insert(int num)
+    {
+        Node * node=root;
+        for(int i=31;i>=0;i--)
+        {   
+            int bit=(num>>i)&1;
+            if(!node->links[bit])
             {
-                int bit=num>>i&1;
-                if(!node->links[bit])
-                {
-                    node->links[bit]=new Node();
-                }
-                node=node->links[bit];
+                node->links[bit]=new Node();
             }
+            node=node->links[bit];
         }
+    }
 
-
-    int getmax(int x)
+    int getmax(int num)
     {
         Node * node=root;
         int ans=0;
-
-     
         for(int i=31;i>=0;i--)
         {
-            int bit=(x>>i)&1;
+            int bit=(num>>i)&1;
             int ops=1-bit;
             if(node->links[ops])
             {
@@ -63,26 +48,28 @@ public:
         }
         return ans;
     }
+
     vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
         sort(nums.begin(),nums.end());
         vector<array<int,3>>q;
-
         for(int i=0;i<queries.size();i++)
         {
-            q.push_back({queries[i][1],queries[i][0],i});
+            int m=queries[i][1];
+            int x=queries[i][0];
+            q.push_back({m,x,i});
         }
         sort(q.begin(),q.end());
-        vector<int>res(queries.size());
         int idx=0;
-        for(auto &[m,x,qi]:q)
+        vector<int>res(queries.size());
+        for(auto &[m,x,i]:q)
         {
             while(idx<nums.size()&&nums[idx]<=m)
             {
                 insert(nums[idx]);
                 idx++;
             }
-            if(idx==0)res[qi]=-1;
-            else res[qi]=getmax(x);
+            if(idx==0)res[i]=-1;
+            else res[i]=getmax(x);
         }
         return res;
     }
