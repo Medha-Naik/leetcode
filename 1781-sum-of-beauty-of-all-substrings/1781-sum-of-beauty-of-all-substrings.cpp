ae48bfb21718d858851ecdf2185994d136c17002
Medha-Naik <medham.23.becs@acharya.ac.in>
2026-02-16 22:30:00 +0530

    Time: 75 ms (84.62%), Space: 11.1 MB (63.59%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n=s.size();
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            vector<int>freq(26,0);
+            int max_freq=0;
+            for(int j=i;j<n;j++)
+            {
+                int idx=s[j]-'a';
+                freq[idx]++;
+                max_freq=max(max_freq,freq[idx]);
+                int min_freq=INT_MAX;
+                for(int k=0;k<26;k++)
+                {
+                    if(freq[k]>0)
+                    {
+                        min_freq=min(min_freq,freq[k]);
+                    }
+                }
+                res+=(max_freq-min_freq);
+            }
+        }
+        return res;
+    }
+
+};
