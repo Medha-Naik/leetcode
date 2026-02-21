697c923e75f6eae30e75bf6a20180ba1c55e2992
Medha-Naik <medham.23.becs@acharya.ac.in>
2026-02-21 23:54:34 +0530

    Time: 3 ms (83.97%), Space: 9.9 MB (45.03%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -6,17 +6,19 @@ public:
         {
             string curr="";
             int cnt=1;
-            for(int j=1;j<res.size();j++)
+            int j=1;
+            while(j<res.size())
             {
                 if(res[j-1]==res[j])cnt++;
                 else{
-                    curr+=to_string(cnt)+res[j-1];
-                    cnt=1;
+                curr+=to_string(cnt)+res[j-1];
+                cnt=1;
                 }
+                j++;
             }
-            curr+=to_string(cnt)+res.back();
+            curr+=to_string(cnt)+res[j-1];
             res=curr;
         }
-        return res;
+    return res;
     }
 };
