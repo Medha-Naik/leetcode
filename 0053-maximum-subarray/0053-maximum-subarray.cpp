1574facc22a7208c1250d75c9b4dd3b667af15d1
Medha <medham.23.becs@acharya.ac.in>
2026-02-26 23:26:50 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.12%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,24 +3,12 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        int start=0;
-        int astart=-1,aend=-1;
         int maxi=INT_MIN;
-        
         for(int i=0;i<n;i++)
-        {   sum+=nums[i];
-             if(sum>maxi)
-            {
-                maxi=sum;
-                astart=start;
-                aend=i;
-            }
-            if(sum<0)
-            {
-                sum=0;
-                start=i+1;
-            }
-           
+        {
+            if(sum<0)sum=0;
+            sum+=nums[i];
+            maxi=max(maxi,sum);
         }
         return maxi;
     }
