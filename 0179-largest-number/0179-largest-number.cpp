edf85824d259767983dfb2072331fd606b15be57
Medha <medham.23.becs@acharya.ac.in>
2026-04-22 00:22:55 +0530

    Time: 4 ms (52.14%), Space: 16.2 MB (86.6%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),[](int a,int b)
+        {return to_string(a)+to_string(b)>to_string(b)+to_string(a);
+        });
+        string res;
+        for(auto s:nums)
+        {
+            res+=to_string(s);
+        }
+        if(res[0]=='0')return "0";
+        return res;
+    }
+};
