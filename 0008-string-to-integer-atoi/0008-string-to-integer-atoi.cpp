3519ec19b208457017118d8149fba7e61fc2f412
Medha <medham.23.becs@acharya.ac.in>
2026-04-29 00:24:13 +0530

    Time: 0 ms (100%), Space: 9.1 MB (80.85%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n=s.size();
-        int i=0;
-        while(i<n&&s[i]==' ')
-        {
+        int n = s.size();
+        int i = 0;
+        int ans = 0;
+        int isneg = 1;
+
+        while (i < n && s[i] == ' ') {
             i++;
         }
-        int sign=1;
-        if(i<n&&(s[i]=='-'||s[i]=='+'))
-        {
-            if(s[i]=='-')
-            {
-                sign=-1;
+
+        if (i < n && s[i] == '-') {
+            isneg = -1;
+            i++;
+        }
+        else if (i < n && s[i] == '+') {
+            i++;
+        }
+
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            if (ans > (INT_MAX - digit) / 10) {
+                return (isneg == -1) ? INT_MIN : INT_MAX;
             }
+
+            ans = ans * 10 + digit;
             i++;
         }
-        long long res=0;
-        while(i<n&&isdigit(s[i]))
-        {
-            
-            int digit=s[i]-'0';
-            if(res>(INT_MAX-digit)/10)
-            {
-                return sign==1? INT_MAX:INT_MIN;
-            }
-            res=res*10+digit;
-            i++;
-        }
-        return sign*res;
+
+        return ans * isneg;
     }
 };
