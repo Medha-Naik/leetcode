3558895cc5a3ba4479bc1e2b88062849a37d2ef7
Medha <medham.23.becs@acharya.ac.in>
2026-04-30 14:47:12 +0530

    Time: 0 ms (100%), Space: 14.2 MB (40.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -7,9 +7,9 @@ public:
             res.push_back(curr);
             return;
         }
-        if(target<0||ind==n)return;
         for(int i=ind;i<n;i++)
-        {
+        {   
+            if(candidates[i]>target)return;
             curr.push_back(candidates[i]);
             findcombs(candidates,target-candidates[i],i,n,curr,res);
             curr.pop_back();
@@ -20,6 +20,7 @@ public:
         int n=candidates.size();
         vector<int>curr;
         vector<vector<int>>res;
+        sort(candidates.begin(),candidates.end());
         findcombs(candidates,target,0,n,curr,res);
         return res;
     }
