6708118450375de45f8f391a9694a16ba8c78604
Medha <medham.23.becs@acharya.ac.in>
2026-06-29 00:31:26 +0530

    Time: 0 ms (100%), Space: 8.1 MB (25.78%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -3,9 +3,13 @@ public:
     int minBitFlips(int start, int goal) {
         int ans=start^goal;
         int cnt=0;
-        for (int i=0;i<=31;i++)
+        for(int i=0;i<=31;i++)
         {
-            if(ans&(1<<i))cnt+=1;
+            if(ans&1)
+            {
+                cnt+=1;   
+            }
+            ans>>=1;
         }
         return cnt;
     }
