726fd578204d834519cc79ff44427fb83d99e1f1
Medha <medham.23.becs@acharya.ac.in>
2026-07-01 23:42:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans=1;
         long long N=n;
-        if(N<0){
-            x=1/x;
+        if(n<0)
+        {
+            x=(1/x);
             N=-N;
         }
+        double res=1;
         while(N>0)
         {
-           if(N&1)
-           {
-            ans*=x;
-
-           }
-           x*=x;
-           N>>=1;
+            if(N%2==1)
+            {
+                res=double(res)*double(x);
+                N--;
+            }
+            else{
+                x*=x;
+                N>>=1;
+            }
         }
-        return ans;
+        return res;
     }
 };
