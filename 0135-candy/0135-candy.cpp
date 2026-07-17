be3659025f6d2d02d67a82b46ab0d323b2a2c48c
Medha <medham.23.becs@acharya.ac.in>
2026-07-18 00:16:24 +0530

    Time: 0 ms (100%), Space: 22 MB (91.67%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        if (n <= 1) return n;
+        
+        int i = 1, sum = 1;
+        
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                sum += 1;
+                i++;
+                continue;
+            }
+            
+            int peak = 1;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                sum += peak;
+                i++;
+            }
+            
+            int down = 0;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                down++;
+                sum += down;
+                i++;
+            }
+            
+            if (down >= peak) {
+                sum += (down - peak + 1);
+            }
+        }
+        return sum;
+    }
+};
