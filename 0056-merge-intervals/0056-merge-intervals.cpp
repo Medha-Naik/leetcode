53f65360ef3aae8de50bff24ea804a16b5d7380d
Medha <medham.23.becs@acharya.ac.in>
2026-07-20 19:53:50 +0530

    Time: 3 ms (88.82%), Space: 23.8 MB (82.43%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        if(n<2)return intervals;
+        vector<vector<int>>res;
+        sort(intervals.begin(),intervals.end());
+        res.push_back(intervals[0]);
+        for(int i=1; i<n; i++)
+        {
+            if(intervals[i][0]>res.back()[1])
+            {
+                res.push_back(intervals[i]);
+            }
+            else{
+                res.back()[1]=max(intervals[i][1],res.back()[1]);
+            }
+
+        }
+        return res;
+    }
+};
