d87f21192834e09c7feee05db7cd93d05e318689
Medha <medham.23.becs@acharya.ac.in>
2026-07-22 00:26:47 +0530

    Time: 0 ms (100%), Space: 17 MB (39.94%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,48 +2,51 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>vis(m,vector<int>(n,0));
-        int steps=0;
-        queue<pair<pair<int,int>,int>>q;
+        int n= grid[0].size();
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        queue<pair<int,int>>q;
+        int freshCount =0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==2)
                 {
-                    q.push({{i,j},0});
-                    vis[i][j]=1;
+                    q.push({i,j});
+                    visited[i][j]=1;
                 }
-                else if(grid[i][j]==0)vis[i][j]=1;
+                else if(grid[i][j]==1)freshCount++;
             }
         }
-        int dx[4]={0,-1,0,+1};
-        int dy[4]={-1,0,+1,0};
-    
-        while(!q.empty())
-        {
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            steps=q.front().second;
-            q.pop();
-             for(int i=0;i<4;i++){
-                int nrow=row+dx[i];
-                int ncol=col+dy[i];
-                if(nrow>=0&&nrow<m&&ncol>=0&&ncol<n&&!vis[nrow][ncol]&&grid[nrow][ncol]==1)
-                {
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},steps+1});
-                }
-                }
-            
 
-        }
-        for(int i=0;i<m;i++)
+        if(freshCount ==0) return 0;
+
+        int mins =0;
+        int row[4]={0,-1,0,+1};
+        int col[4]={-1,0,+1,0};
+        while(!q.empty()&& freshCount>0)
         {
-            for(int j=0;j<n;j++)
-                if(!vis[i][j])return -1;
+            int s=q.size();
+            mins++;
+            while(s--)
+            {
+                int x=q.front().first;
+                int y=q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++)
+                {
+                    int r=x+row[i];
+                    int c=y+col[i];
+
+                    if(r>=0&&c>=0&&r<m&&c<n&&!visited[r][c]&&grid[r][c]==1)
+                    {
+                        visited[r][c]=1;
+                        freshCount --;
+                        q.push({r,c});
+                    }
+                }
+            }
         }
-        return steps;
+        return freshCount==0?mins:-1;
     }
 };
