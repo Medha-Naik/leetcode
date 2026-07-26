02547b4c9d09972a94337e0f2505954406bb3d01
Medha <medham.23.becs@acharya.ac.in>
2026-07-26 12:55:40 +0530

    Time: 19 ms (53.24%), Space: 26.3 MB (37.06%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m=grid.size();
-        if(grid[0][0]||grid[m-1][m-1])return -1;
+    int row[8]={-1,-1,-1,0,0,+1,+1,+1};
+    int col[8]={-1,0,+1,-1,+1,-1,0,+1};
+
+    int shortestDist(vector<vector<int>>&grid,int m,int n)
+    {
         queue<pair<int,pair<int,int>>>q;
         q.push({1,{0,0}});
-        
-        vector<vector<int>>dist(m,vector<int>(m,INT_MAX));
-        dist[0][0]=1;
-        int dx[8]={-1,-1,-1,0,0,+1,+1,+1};
-        int dy[8]={-1,0,+1,-1,+1,-1,0,+1};
+        vector<vector<int>>visited(m,vector<int>(n,INT_MAX));
+
         while(!q.empty())
         {
-            int dis=q.front().first;
-            int row=q.front().second.first;
-            int col=q.front().second.second;
+            auto it=q.front();
+            int dist=it.first;
+            int r=it.second.first;
+            int c=it.second.second;
             q.pop();
-            if(row==m-1&&col==m-1)return dis;
+
+            if(r==m-1 && c==n-1)return dist;
+            
             for(int i=0;i<8;i++)
             {
-                int nrow=row+dx[i];
-                int ncol=col+dy[i];
-                if(nrow<m&&nrow>=0&&ncol<m&&ncol>=0&&grid[nrow][ncol]==0&&dist[nrow][ncol]>dis+1)
+                int x=r+row[i];
+                int y=c+col[i];
+                if( x>=0&& x<m && y>=0 && y<n&& grid[x][y]==0&&visited[x][y]>dist+1)
                 {
-                    dist[nrow][ncol]=dis+1;
-                    q.push({dis+1,{nrow,ncol}});
+                    visited[x][y]=dist+1;
+                    q.push({dist+1,{x,y}});
                 }
+
             }
         }
-        return -1;
+    return -1;
+    }
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+
+        if(grid[0][0]==1||grid[m-1][n-1]==1)return -1;
+        return shortestDist(grid,m,n);
     }
 };
