30b2aaeda9ac786fd569a3df7df6ae9695463384
Medha <medham.23.becs@acharya.ac.in>
2026-07-29 23:26:04 +0530

    Time: 0 ms (100%), Space: 8.2 MB (96.66%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int open=0;
-        int close=0;
-        int n=s.size();
-        for(int i=0;i<n;i++)
+        int open=0, close =0;
+
+        for(char ch:s)
         {
-            if(s[i]=='(')open++;
-            else if(s[i]==')'&&open==0)close++;
-            else{
-                open--;
-            }
+           if(ch=='(')open++;
+           else {
+            if(open>0)open--;
+            else close++;
+           }
         }
         return open+close;
     }
