45bcfd4c9105e01de0cddeb2caebbfd581f367c7
Medha <medham.23.becs@acharya.ac.in>
2026-07-30 10:54:26 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
         int j=-1;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
             if(nums[i]==0)
-            {
-                j=i;
-                break;
+            { j=i;
+            break;
             }
         }
         if(j==-1)return;
-        for(int i=j+1;i<nums.size();i++)
+        for(int i=j+1;i<n;i++)
         {
             if(nums[i]!=0)
             {
@@ -19,6 +19,5 @@ public:
                 j++;
             }
         }
-        
     }
 };
