cf807bcfffbfe6b3409cd379c00b4d0cde9e73a9
Medha <medham.23.becs@acharya.ac.in>
2026-07-30 22:38:19 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.36%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int singlenum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            singlenum^=nums[i];
+        }
+        return singlenum;
+    }
+};
