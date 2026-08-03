aba912d83f12def9a519c9b47503a30bde64e1c2
Medha <medham.23.becs@acharya.ac.in>
2026-08-03 19:56:34 +0530

    Time: 3 ms (35.49%), Space: 112.6 MB (8.82%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n= gas.size();
-        int curgas=0;
-        int start=0;
-        int total=0;
-        for(int i=0;i<n;i++)
+        int totalgas = accumulate(gas.begin(), gas.end(),0);
+        int totalcost = accumulate(cost.begin(), cost.end(),0);
+        if(totalgas<totalcost)return -1;
+        int n = gas.size();
+        int curgas= 0;
+        int start =0;
+        for(int i =0; i<n; i++)
         {
-            int diff=gas[i]-cost[i];
-            curgas+=diff;
-            total+=diff;
+            curgas+=gas[i]- cost[i];
             if(curgas<0)
             {
                 curgas=0;
-                start=i+1;
+                start = i+1;
             }
-
         }
-        return (total<0)?-1:start;
+        return start;
     }
 };
