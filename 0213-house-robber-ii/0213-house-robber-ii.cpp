b5b830349a4348924b40f37ee8eb8495d7f8b047
Medha <medham.23.becs@acharya.ac.in>
2026-08-05 07:10:10 +0530

    Time: 0 ms (100%), Space: 10.2 MB (74.65%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    int robLinear(vector<int>& nums, int start, int end) {
+        int prev = 0;
+        int prev2 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int take = nums[i] + prev2;
+            int nottake = prev;
+            
+            int cur = max(take, nottake);
+            prev2 = prev;
+            prev = cur;
+        }
+        return prev;
+    }
+
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        int case1 = robLinear(nums, 0, n - 2);
+        
+        int case2 = robLinear(nums, 1, n - 1);
+
+        return max(case1, case2);
+    }
+};
