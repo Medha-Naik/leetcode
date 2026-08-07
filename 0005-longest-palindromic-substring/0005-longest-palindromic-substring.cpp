4d47588a2a7404c61c2160fe2e8e705d709d2d54
Medha <medham.23.becs@acharya.ac.in>
2026-08-07 14:31:56 +0530

    Time: 18 ms (41.66%), Space: 22.3 MB (28.74%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -4,33 +4,31 @@ public:
         int n = s.size();
         int reslen =0;
         string res;
-        for(int i =0; i< n; i++)
+        for(int i =0; i<n; i++)
         {
-            int l=i,r=i;
-            while(l>=0 && r<= n-1 && s[l]==s[r])
+            int l = i, r = i;
+            while(l>=0 &&r<n&& s[l] == s[r])
             {
                 if(r-l+1>reslen)
                 {
                     reslen = r-l+1;
-                    res = s.substr(l,r-l+1);
+                    res = s.substr(l,reslen);
                 }
                 l--;
                 r++;
             }
 
-            l =i, r = i+1;
-            while(l>=0&& r<=n-1&& s[l]==s[r])
+            l =i, r=i+1;
+           while(l>=0 &&r<n&& s[l] == s[r])
             {
                 if(r-l+1>reslen)
                 {
                     reslen = r-l+1;
-                    res = s.substr(l,r-l+1);
+                    res = s.substr(l,reslen);
                 }
                 l--;
                 r++;
             }
-
-
         }
         return res;
     }
