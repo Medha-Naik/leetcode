1e4d33b2c342d2000006b8806d23ed992f690c77
Medha <medham.23.becs@acharya.ac.in>
2026-08-10 19:08:01 +0530

    Time: 18 ms (52.55%), Space: 46.7 MB (50.29%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,58 +1,71 @@
-class disjointSet{
-    public:
-        vector<int>parent,size;
-        disjointSet(int n)
+class DisjointSet{
+    
+
+public:
+vector<int>parent,size;
+    DisjointSet(int n)
+    {
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i =0; i<=n;i++)
         {
-            parent.resize(n);
-            size.resize(n,1);
-            for(int i=0;i<n;i++)
-            {
-                parent[i]=i;
-            }
+            parent[i] = i;
         }
-        int findparent(int u)
+    }
+
+    int findParent(int u)
+    {
+        if(parent[u]== u) return u;
+        return parent[u] = findParent(parent[u]);
+    }
+    void unionbySize(int u, int v)
+    {
+        int par_v = findParent(v);
+        int par_u = findParent(u);
+        if(par_v == par_u)return;
+        if(size[par_v]> size[par_u])
         {
-            if(parent[u]==u)return u;
-            return parent[u]=findparent(parent[u]);
+            size[par_v]+= size[par_u];
+            parent[par_u]= par_v;
         }
-        void unionbySize(int u,int v)
+        else 
         {
-            int pou=findparent(u);
-            int pov=findparent(v);
-            if(pou==pov)return;
-            else if(size[pou]<size[pov])
-            {
-                parent[pou]=pov;
-                size[pov]+=size[pou];
-            }
-            else{
-                parent[pov]=pou;
-                size[pou]+=size[pov];
-            }
+            size[par_u]+= size[par_v];
+            parent[par_v]= par_u;
         }
+    }
 };
 
 class Solution {
 public:
+
+
+
     int makeConnected(int n, vector<vector<int>>& connections) {
-        disjointSet ds(n);
-        int cntext=0;
-        for(auto edge:connections)
+        int component = 0;
+        int extra =0;
+        DisjointSet ds(n);
+        for(auto c: connections)
         {
-            int u=edge[0];
-            int v=edge[1];
-            if(ds.findparent(u)==ds.findparent(v))cntext++;
+            int u = c[0];
+            int v = c[1];
+            if(ds.findParent(u) != ds.findParent(v))
+            {
+                ds.unionbySize(u, v);
+            }
             else{
-                ds.unionbySize(u,v);
+                extra++;
             }
         }
-        int comps=0;
-        for(int i=0;i<n;i++)
+
+        for(int i =0; i<n ; i++)
         {
-            if(ds.parent[i]==i)comps++;
+            if(ds.parent[i]== i)
+            {
+                component++;
+            }
         }
-        int ans=comps-1;
-        if(ans<=cntext)return ans;
-        return -1;
+
+        return (extra>= component -1)? component-1 :-1;
     }
 };
